cc82a983e477a534c28b6c03b1c349dcfe18f78c
郭嘉树 <132477874+GuoJiashu@users.noreply.github.com>
2023-05-07 05:15:36 +1000

    Update i.py

--- i.py
+++ i.py
@@ -3,34 +3,30 @@ import matplotlib.pyplot as plt
 
 
 def is_point_in_ellipse(x, y, a, b, x0, y0, thickness):
-    # Calculate distance from point (x,y) to center (x0,y0)
     r = np.sqrt((x - x0) ** 2 + (y - y0) ** 2)
 
-    # Calculate major and minor axis lengths of outer and inner ellipses
     a_outer = a + thickness / 2
     b_outer = b + thickness / 2
     a_inner = max(a - thickness / 2, 0)
     b_inner = max(b - thickness / 2, 0)
 
-    # Check whether point is inside outer ellipse and outside inner ellipse
     value_outer = (((x - x0) ** 2) / (a_outer ** 2)) + (((y - y0) ** 2) / (b_outer ** 2)) - 1
     value_inner = (((x - x0) ** 2) / (a_inner ** 2)) + (((y - y0) ** 2) / (b_inner ** 2)) - 1
     if value_outer < 0 < value_inner:
-        return -1  # Point is inside ellipse but outside line
+        return -1
     elif value_outer == 0 or value_inner == 0:
-        return 0  # Point is on ellipse or line
+        return 0
     else:
-        return 1  # Point is outside ellipse and line
+        return 1
 
 
-# Define ellipse parameters
 a = 12.5
 b = 7.5
 x0 = 0
 y0 = 7.5
 thickness = 1.5
 
-# Define range of x and y values to evaluate
+# Define range of x and y
 x_min, x_max = -15, 15
 y_min, y_max = -2.5, 17.5
 n_points = 1000
@@ -43,10 +39,11 @@ for i, x in enumerate(x_range):
     for j, y in enumerate(y_range):
         result[j, i] = is_point_in_ellipse(x, y, a, b, x0, y0, thickness)
 
-# Plot the results
+# Plot
 plt.imshow(result, extent=[x_min, x_max, y_min, y_max], cmap='coolwarm', origin='lower')
 plt.colorbar()
 plt.xlabel('x (cm)')
 plt.ylabel('y (cm)')
 plt.title('Boundary of Line')
 plt.show()
+plt.savefig('Boundart of Line')
